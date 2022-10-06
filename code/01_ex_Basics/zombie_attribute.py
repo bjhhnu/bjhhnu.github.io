685c0470bbc6e265e61725604e14da46d67d4ada
Burkhard Hoppenstedt <burkhard.hoppenstedt@exxcellent.de>
2022-10-06 21:54:23 +0200

    feat(content): Code, Uebung

---
+++ code/01_ex_Basics/zombie_attribute.py
@@ -0,0 +1,16 @@
+class Zombie:
+
+   # Klassenvariable
+   kind = 'standard-level'
+
+   def __init__(self, name):
+            # Instanzvariable
+           self.name = name
+
+frankenstein = Zombie('Frankenstein')
+print(frankenstein.name)
+# >>> Frankenstein
+
+walking_dead = Zombie('Walking Dead')
+print(frankenstein.kind)
+print(walking_dead.kind)
---
+++ code/01_ex_Basics/zombie_plain.py
@@ -0,0 +1,15 @@
+# Klasse
+class Zombie:
+
+   # Konstruktor
+   def __init__(self):
+           pass
+
+
+# Instanz
+frankenstein = Zombie()
+walking_dead = Zombie()
+print(frankenstein)
+# >>> <__main__.Zombie object at 0x00000290ABFF6820>
+print(walking_dead)
+# >>> <__main__.Zombie object at 0x00000290ABFEF910>
